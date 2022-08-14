4103ab4b6732432679e4277d9eceb014ec4e4dbd
Sakil <mdsakiluzzaman@gmail.com>
2022-08-14 19:07:30 +0600

    some changes

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -1,13 +1,9 @@
- @extends('layouts.app')
-@section('content') 
+@extends('layouts.app')
+@section('content')
     {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> --}}
 
     <div class="nav-item dropdown">
-      {{-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-          {{ Auth::user()->name }}
-      </a> --}}
-
-      <div>
+        {{-- <div>
           <a class="dropdown-item" href="{{ route('logout') }}"
              onclick="event.preventDefault();
                            document.getElementById('logout-form').submit();">
@@ -17,8 +13,15 @@
           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
               @csrf
           </form>
-      </div>
-
-      
-    
-@endsection
+      </div> --}}
+        <div class="container">
+            <div class="card" style="width:400px">
+                <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+                <div class="card-body">
+                    <h4 class="card-title">John Doe</h4>
+                    <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                    <a href="#" class="btn btn-primary">See Profile</a>
+                </div>
+            </div>
+        </div>
+    @endsection
