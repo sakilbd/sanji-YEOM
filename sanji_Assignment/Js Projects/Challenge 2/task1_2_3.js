73e03649866e2380f2a64b4438af79f0b4bae906
unknown <mdsakiluzzaman@gmail.com>
2022-08-15 11:35:07 +0600

    some changes

---
+++ sanji_Assignment/Js Projects/Challenge 2/task1_2_3.js
@@ -0,0 +1,27 @@
+const taxableIncomes = [35000, 45000, 50000, 62000, 70500, 82500, 97000, 101000, 132000, 150000]
+let taxes = []
+let netIncome = []
+
+
+function calTax(taxableIncomes) {
+    let tax, nIncome;
+    for (i of taxableIncomes) {
+        if (i > 60000 && i < 100000) {
+            tax = (i * 40) / 100;
+            Income = i - tax;
+            taxes.push(tax);
+            netIncome.push(Income);
+        } else {
+            tax = (i * 35) / 100;
+            Income = i - tax;
+            taxes.push(tax);
+            netIncome.push(Income);
+        }
+    }
+}
+
+
+calTax(taxableIncomes)
+
+console.log(taxes)
+console.log(netIncome)
