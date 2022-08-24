83a3b747af18c6d7f90b68c6285de8b1a194122b
Sakil <mdsakiluzzaman@gmail.com>
2022-08-24 13:28:29 +0600

    some changes

--- resources/views/pages/restaurant_list.blade.php
+++ resources/views/pages/restaurant_list.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 @section('content')
-{{ Auth::user()->name }}
-    <div clas="">
+    {{ Auth::user()->name }}
+    <div class="container">
         <div class="cafes">
             <h1 class="text-dark">University of Tasmania's Cafe</h1>
-            <div class="cafe-names">
-                @foreach($restaurant_list as $restaurants)
-                <div class="cafe-section">
-                    <a href="">
-                        <h2>{{ $restaurants->name }}</h2>
+            <div class="cafe-names col-md-12">
+                {{-- @foreach ($restaurant_list->chunk(3) as $restaurant_list) --}}
+                <div class="row">
+                    @foreach ($restaurant_list as $restaurants)
+                    <div class="col-md-4">
+                       
+                            <div class="cafe-section">
+                                <a href="">
+                                    <h2>{{ $restaurants->name }}</h2>
 
-                        <img src={{ $restaurants->image }} />
-                    </a>
+                                    <img src={{ $restaurants->image }} />
+                                </a>
+                            </div>
+                       
+                    </div>
+                    @endforeach
                 </div>
-                @endforeach
+                {{-- @endforeach --}}
 
-                
             </div>
         </div>
     </div>
