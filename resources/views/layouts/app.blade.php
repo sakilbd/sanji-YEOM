3bd101a07e7befced9be5fccdbd2e63eeb16fbf6
Sakil <mdsakiluzzaman@gmail.com>
2022-08-24 16:35:30 +0600

    user profile and name added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,18 +13,19 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/restaurantStyles/design.css') }}" >
+    <link rel="stylesheet" href="{{ asset('css/restaurantStyles/design.css') }}">
     @yield('styles')
-    
+
     {{-- <link href="{{ asset('restaurantStyles/design.css') }}" rel="stylesheet"> --}}
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -31,7 +33,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -57,9 +61,9 @@
                                 </li>
                             @endif
                         @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('/')}}">Home</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('/') }}">Home</a>
+                            </li>
                             {{-- <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
@@ -77,54 +81,77 @@
                                     </form>
                                 </div>
                             </li> --}}
-                            <?php if((Auth::user())){
+                            <?php if (Auth::user()) {
                                 $user = Auth::user()->role_name;
-                            } 
+                            }
                             ?>
-                            @if(isset($user))
-                            @if($user == 'admin')
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{url('restaurant-list')}}">Restaurant List</a>
-                            </li>
-                            
-                            @endif
+                            @if (isset($user))
+                                @if ($user == 'admin')
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ url('restaurant-list') }}">Restaurant List</a>
+                                    </li>
+                                @endif
                             @endif
                         @endguest
+
                     </ul>
-                    <?php if((Auth::user())){
-                        $user = Auth::user()->role_name;
-                    } 
+                    <?php if (Auth::user()) {
+                        $user = Auth::user();
+                    }
                     ?>
-                  
-                    <div class="nav-item dropdown">
-                        {{-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    @if (Auth::check())
+                        <div class="nav-item dropdown">
+                            {{-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
                         </a> --}}
 
-                        <div>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
+                            <div>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
+                                    {{ __('Logout') }}
+                                </a>
 
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
                         </div>
+                    @endif
+                    <div class="nav-item ml-auto ">
+                        {{-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a> --}}
+
+                        @if (isset($user))
+                            <div class="" href="{{ route('logout') }}">
+                                {{ $user->name }} &nbsp<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                                    fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                    <path fill-rule="evenodd"
+                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                </svg>
+                            </div>
+                        @endif
+
+
+
                     </div>
+
+
                 </div>
             </div>
         </nav>
-     
+
         <main class="py-4">
             @yield('content')
         </main>
-      
+
     </div>
-    
+
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 
 </body>
+
 </html>
